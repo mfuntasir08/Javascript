2f8ff1fbc267d82589aaabe148959c51701cf741
mfuntasir08 <muntasirislamfahim18@gmail.com>
2025-07-13 18:57:22 +0600

    2ND COMMIT

---
+++ script.js
@@ -0,0 +1,85 @@
+//arithmatic operators
+//let a = 5;
+//let b = "3";
+
+//console.log("a + b =",a + b);
+//console.log("b ** a =",b ** a);//a^b
+//console.log("a % b =",a % b);//remainder = 2
+
+//console.log("a >= b =",a = b);
+
+//conditional statements
+
+// let age = 18 ;
+// if (age>=18){
+// console.log("You can Vote")
+// }
+
+// if (age<18){
+// console.log("You can not Vote")
+// }
+
+// let mode = "Blue"
+// let color;
+
+// if (mode==="Dark"){
+
+// color = "Black"
+
+// }
+
+// else{ 
+
+// color = "White";
+
+// }
+// console.log(color);
+//  let num = 64830;
+ 
+//  if (num % 2===0){
+
+//   console.log(num,'is Even')
+
+//  } else {
+
+// console.log(num,`is Odd`)
+
+//  }
+//syntax->rules
+//  let mode="green"
+// let color;
+// if (mode==="dark"){
+
+// color = "black"
+
+// } else if(mode==="blue") {
+// color="blue"
+
+// }else if(mode==="green") {
+// color="green"
+
+// }else if(mode==="pink") {
+// color="pink"
+
+// }
+// else {
+
+//   color = "white"
+// }
+// console.log(color)
+
+// let age = 9;
+
+//  age >= 18 ? console.log("voter :","Yes"):console.log("voter :","No");//compact if-else
+// let num = prompt ("Write your age");
+// console.log("Your age is:",num)
+// if (num>=18){
+// console.log("You can vote")
+// } else{
+//   console.log("You Can not vote")
+//}
+
+
+
+
+
