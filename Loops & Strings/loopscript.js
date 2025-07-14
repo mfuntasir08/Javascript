aa2cd3c076b70f0243752a3c599e6488e13e5874
mfuntasir08 <muntasirislamfahim18@gmail.com>
2025-07-15 01:02:33 +0600

    Practice: added loops exercise

--- Loops & Strings/loopscript.js
+++ Loops & Strings/loopscript.js
@@ -1,19 +1,53 @@
 
-let i=1;
-do {
-  console.log("i=",i);
-  i++;
-} while(i<=10);
-let str = "1234567890";
+// let i=1;
+// do {
+//   console.log("i=",i);
+//   i++;
+// } while(i<=10);
+// let str = "1234567890";
 
-let size = 0;
+// let size = 0;
 
- for(let val of str){
-  console.log("val=",val);
-  size++;
- }
+//  for(let val of str){
+//   console.log("val=",val);
+//   size++;
+//  }
 
-console.log("string size=",size);
+// console.log("string size=",size);
+
+//for in loop
+
+// let student = {
+// name : "Muntasir Fahim",
+// age : 19,
+// gpa : 5.00,
+// ispass : true,
+// };
+// for ( let i in student ){
+//   console.log(i);
+// }
+
+// practice qs 1:print all even numbers from 0 to 100 
+
+// for (let i = 0;i <= 100; i++){
+
+// if(i % 2 !== 0) {
+
+// console.log(i);
+// }
+// }
+// practice qs 2:
+
+let gameNum = 46;
+ 
+let userNum = prompt("Guess the game num :");
+
+while(userNum != gameNum) { //game
+ userNum  = prompt("you have entered the wrong number.Guess again:");
+}
+
+console.log("congratualations,you have entered the right number");
+
 
 
 
