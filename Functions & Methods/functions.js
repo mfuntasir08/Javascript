ff34beca3a4d7a124a35f15b1548a593e4b30f7a
mfuntasir08 <muntasirislamfahim18@gmail.com>
2025-07-21 00:14:05 +0600

    started to use functions

--- Functions & Methods/functions.js
+++ Functions & Methods/functions.js
@@ -1,8 +1,29 @@
 // console.log("hello!");
- function myFunction(){
-  console.log("Welcome Home")
-  console.log("we are learning JS")
- }
-myFunction();
+//  function myFunction(){
+//   console.log("Welcome Home")
+//   console.log("we are learning JS")
+//  }
+// myFunction();
+//  function myFunction(msg,n){ 
+//   // parameter-->input
+//   console.log(msg + n)
+//  }
 
+// myFunction("js is easy",90); //argument
+
+//function-->2 numbers,sum
+
+// function sum(x, y){
+// console.log(x+y);
+// }
+
+// sum(1,68);
+
+function sum(x, y){
+s = x+y;
+return s;
+}
+
+let val = sum(3,5);
+console.log(val);
 
